04ee2a970fcd2abd5f558a5b872a8eea3c8ff0ec
珍珍 <447185395@qq.com>
2026-04-10 10:53:06 +0800

    First commit

---
+++ src/main/java/org/example/hong/HongApplication.java
@@ -0,0 +1,13 @@
+package org.example.hong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hong/HongApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
